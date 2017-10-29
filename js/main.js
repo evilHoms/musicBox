eeb9b843300fa5f1b239dd54d863aa0708956c1b
Igor Tciva <homspage@gmail.com>
2017-10-29 19:42:03 +0300

    KeyDown listeners added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 const notes = document.querySelectorAll(`.note`);
 
+console.log(notes);
+
+window.addEventListener(`keydown`, musicKeyDown);
 
 notes.forEach(note => {
   note.addEventListener(`click`, (e) => {
@@ -9,7 +12,42 @@ notes.forEach(note => {
   });
 });
 
+
+function musicKeyDown(e) {
+  switch(e.keyCode) {
+    case 65:
+      console.log(`a pressed`);
+      btnClick(notes[0]);
+      break;
+    case 83:
+      console.log(`s pressed`);
+      btnClick(notes[1]);
+      break;
+    case 68:
+      console.log(`d pressed`);
+      btnClick(notes[2]);
+      break;
+    case 70:
+      console.log(`f pressed`);
+      btnClick(notes[3]);
+      break;
+    case 74:
+      console.log(`j pressed`);
+      btnClick(notes[4]);
+      break;
+    case 75:
+      console.log(`k pressed`);
+      btnClick(notes[5]);
+      break;
+    case 76:
+      console.log(`l pressed`);
+      btnClick(notes[6]);
+      break;
+  }
+}
+
 function btnClick(note, e) {
+  console.log(note);
   const audio = document.querySelector(`audio.${note.id}`);
   audio.currentTime = 0;
   audio.play();
